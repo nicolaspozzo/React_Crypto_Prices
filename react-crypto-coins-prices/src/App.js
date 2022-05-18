d1639e97a3c8453e3253b87f9b34755ed8f28b21
nicolaspozzo <nicolaspozzo19@gmail.com>
2022-05-18 01:45:47 -0300

    agrego conexion con api y envio datos a componente tableCoins

--- react-crypto-coins-prices/src/App.js
+++ react-crypto-coins-prices/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+import TableCoins from './components/TableCoins/TableCoins'
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  const getData = async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1')
+    
+
+    setCoins(res.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row'>
+        <h1>Coin Market</h1>
+        <TableCoins coins={coins}/>
+      </div>
+    
     </div>
   );
 }
