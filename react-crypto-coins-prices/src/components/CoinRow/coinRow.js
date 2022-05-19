260420696a09c244351d5768abd166922274052d
nicolaspozzo <nicolaspozzo19@gmail.com>
2022-05-19 02:14:08 -0300

    construyo cada una de las filas y sus estilos

---
+++ react-crypto-coins-prices/src/components/CoinRow/coinRow.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './CoinRow.css'
+
+function coinRow({ coin, index }) {
+  console.log(coin, index);
+  return (
+    <tr key={coin.id}>
+      <td>{index}</td>
+      <td>
+        <img src={coin.image} alt={coin.name} />
+        <span>{coin.name}</span>
+        <span className="symbol">{coin.symbol}</span>
+      </td>
+      <td>{coin.current_price}</td>
+      <td className={coin.price_change_24h > 0? 'text-success' :'text-danger'} >{coin.price_change_24h}</td>
+      <td>{coin.total_volume}</td>
+    </tr>
+  );
+}
+
+export default coinRow;
