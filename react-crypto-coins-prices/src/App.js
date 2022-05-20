d0821f07a0454199c6821e29ca8b4911a7a42490
nicolaspozzo <nicolaspozzo19@gmail.com>
2022-05-20 18:46:31 -0300

    formateo codigo

--- react-crypto-coins-prices/src/App.js
+++ react-crypto-coins-prices/src/App.js
@@ -1,42 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-import {useEffect, useState} from 'react'
-import TableCoins from './components/TableCoins/TableCoins'
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import TableCoins from "./components/TableCoins/TableCoins";
 
 function App() {
-
-  const [coins, setCoins] = useState([])
-  const [search, setSearch] = useState('')
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   const getData = async () => {
-    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1')
-    
+    const res = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1"
+    );
 
-    setCoins(res.data)
-  }
+    setCoins(res.data);
+  };
 
   useEffect(() => {
     // setInterval(() => {
-      getData()
+    getData();
     // }, 1000)
-  }, [])
+  }, []);
 
-  
   return (
     <div className="container">
-      <div className='row m-5'>
+      <div className="row m-5">
         <div className="d-flex justify-content-between">
           <div>
-            <h1>Coin Market</h1>            
+            <h1>Coin Market</h1>
           </div>
           <div>
-            <input type="text" placeholder='Search a coin' className=' form-control bg-dark text-light border-0 mt-2 text-center' onChange={e => setSearch(e.target.value)}/>            
+            <input
+              type="text"
+              placeholder="Search a coin"
+              className=" form-control bg-dark text-light border-0 mt-2 text-center"
+              onChange={(e) => setSearch(e.target.value)}
+            />
           </div>
         </div>
-        <TableCoins coins={coins} search={search}/>
+        <TableCoins coins={coins} search={search} />
       </div>
-    
     </div>
   );
 }
--- react-crypto-coins-prices/src/components/TableCoins/TableCoins.js
+++ react-crypto-coins-prices/src/components/TableCoins/TableCoins.js
@@ -1,33 +1,32 @@
-import React from 'react'
-import './TableCoins.css'
-import CoinRow from '../CoinRow/coinRow'
+import React from "react";
+import "./TableCoins.css";
+import CoinRow from "../CoinRow/coinRow";
 
+const titles = ["#", "Coin", "Price", "Price Change", "24 Volume"];
 
-const titles = ['#', 'Coin', 'Price', 'Price Change', '24 Volume' ]
+const TableCoins = ({ coins, search }) => {
+  const filteredCoins = coins.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(search.toLowerCase()) |
+      coin.symbol.toLowerCase().includes(search.toLowerCase())
+  );
 
-const TableCoins = ({coins, search}) => {
-    
-    const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) | coin.symbol .toLowerCase().includes(search.toLowerCase()))
-
-
-  return ( 
-    <table className='table table-dark mt-4 table-hover'>
-        <thead>
-            <tr>
-                {
-                    titles.map((title,index) => (
-                        <td key={index}>{title}</td>
-                    ))
-                }
-            </tr>
-        </thead>
-        <tbody>
-           {filteredCoins.map((coin, index) => (
-               <CoinRow coin={coin} index={index + 1} key={index} />
-           ))}
-        </tbody>
+  return (
+    <table className="table table-dark mt-4 table-hover">
+      <thead>
+        <tr>
+          {titles.map((title, index) => (
+            <td key={index}>{title}</td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {filteredCoins.map((coin, index) => (
+          <CoinRow coin={coin} index={index + 1} key={index} />
+        ))}
+      </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default TableCoins
+export default TableCoins;
