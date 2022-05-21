2b2bf3273ed11b8c21f312902be810cc7f06554d
nicolaspozzo <nicolaspozzo19@gmail.com>
2022-05-21 03:15:05 -0300

    obtengo datos historicos de monedas

--- react-crypto-coins-prices/src/App.js
+++ react-crypto-coins-prices/src/App.js
@@ -3,27 +3,57 @@ import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import TableCoins from "./components/TableCoins/TableCoins";
+import CoinChart from './components/CoinChart/CoinChart'
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [historicData, setHistoricData] = useState([]);
+
+
 
   const getData = async () => {
     const res = await axios.get(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1"
     );
-
     setCoins(res.data);
   };
 
+  // const getHistoricChart = async (id = 'bitcoin', days = 365, currency = 'bitcoin') => {  
+  const getHistoricChart = async () => {
+
+    // const res = await axios.get(
+    //   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`
+    // );
+    
+    const res = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=btc&days=365`
+    );
+
+    setHistoricData(res.data.prices)
+    console.log('prices', res.data.prices);
+  };
+
+    useEffect(() => {
+      getHistoricChart();
+    }, []);
+
+
   useEffect(() => {
-    // setInterval(() => {
     getData();
-    // }, 1000)
+    // const interval = setInterval(() => {
+    //   getData();
+    // }, 1000);
+    // return () => clearInterval(interval);
+    
   }, []);
+
+
+     
 
   return (
     <div className="container">
+      <CoinChart historicData={historicData}/>
       <div className="row m-5">
         <div className="d-flex justify-content-between">
           <div>
