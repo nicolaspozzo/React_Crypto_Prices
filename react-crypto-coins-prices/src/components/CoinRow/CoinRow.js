9378b2e1c0f2453ccc1c50683e9c792f07103aea
nicolaspozzo <nicolaspozzo19@gmail.com>
2022-05-22 15:23:38 -0300

    agrego redireccion al grafico de cada moneda

---
+++ react-crypto-coins-prices/src/components/CoinRow/CoinRow.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./CoinRow.css";
+import { useNavigate } from "react-router-dom";
+
+const CoinRow = ({ coin, index }) => {
+
+  let navigate = useNavigate();
+  
+  return (
+    <tr key={coin.id}>
+      <td>{index}</td>
+      <td>
+        <img src={coin.image} alt={coin.name} />
+        <span>{coin.name}</span>
+        <span className="symbol">{coin.symbol}</span>
+      </td>
+      <td>{coin.current_price}</td>
+      <td
+        className={coin.price_change_24h > 0 ? "text-success" : "text-danger"}
+      >
+        {coin.price_change_24h}
+      </td>
+      <td>{coin.total_volume}</td>
+      <td>
+        <button onClick={() => {navigate(`/coin-chart/${coin.name}`)}}>
+          Chart
+        </button>
+      </td>
+    </tr>
+  );
+};
+export default CoinRow;
