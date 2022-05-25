e8e4f26148642c7aad6a226efd8b753d1dc0ee93
nicolaspozzo <nicolaspozzo19@gmail.com>
2022-05-25 02:32:52 -0300

    avances en footer

--- react-crypto-coins-prices/src/components/Footer/Footer.js
+++ react-crypto-coins-prices/src/components/Footer/Footer.js
@@ -3,8 +3,43 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center px-5 ">
+          <a
+            href="/"
+            class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+          >
+            <i class="bi bi-bootstrap"></i>
+          </a>
+          <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex px-5">
+          <li class="ms-3">
+            <a class="text-muted" href="#">
+              <i class="bi bi-twitter"></i>
+            </a>
+          </li>
+          <li class="ms-3">
+            <a class="text-muted" href="#">
+              <i class="bi bi-instagram"></i>
+            </a>
+          </li>
+          <li class="ms-3">
+            <a class="text-muted" href="#">
+              <i class="bi bi-whatsapp"></i>
+            </a>
+          </li>
+          <li class="ms-3">
+            <a class="text-muted" href="#">
+              <i class="bi bi-youtube"></i>
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
 }
 
 export default Footer
