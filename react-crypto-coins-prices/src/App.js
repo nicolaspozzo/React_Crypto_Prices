782443957277cd53349f8be8489ad51d9f9c1b12
nicolaspozzo <nicolaspozzo19@gmail.com>
2022-05-25 02:34:02 -0300

    agrego importacion de componentes

--- react-crypto-coins-prices/src/App.js
+++ react-crypto-coins-prices/src/App.js
@@ -1,10 +1,12 @@
 
 import "./App.css";
 // import 'bootswatch/dist/cyborg/bootstrap.css'
-import { BrowserRouter as Router, Route, Routes, useNavigate, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from './components/Home/Home'
 import ErrorPage from './components/ErrorPage/ErrorPage'
 import CoinChart from "./components/CoinChart/CoinChart";
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
@@ -14,16 +16,13 @@ function App() {
   return (    
       
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/error-page">Error Page</Link>
-      </nav>
+      <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/coin-chart/:coinName" element={<CoinChart />} />
           <Route path="*" element={<ErrorPage />} />
       </Routes>
-      <div>Footer</div>
+        <Footer/>
       </Router>
       
   );
