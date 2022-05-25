efec3153516dcdd86409f0f2907bc561407580be
nicolaspozzo <nicolaspozzo19@gmail.com>
2022-05-25 02:33:43 -0300

    agrego importacion para bootstrap

--- react-crypto-coins-prices/src/index.js
+++ react-crypto-coins-prices/src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
